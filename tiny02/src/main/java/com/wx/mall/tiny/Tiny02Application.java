dfd539450ad4f164b4c58b64ba667b817d2d358c
lyoveu <1228080240@qq.com>
2020-03-03 16:49:08 +0800

    tiny02 redis

---
+++ tiny02/src/main/java/com/wx/mall/tiny/Tiny02Application.java
@@ -0,0 +1,13 @@
+package com.wx.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tiny02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tiny02Application.class, args);
+    }
+
+}
---
+++ tiny02/src/test/java/com/wx/mall/tiny/Tiny02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wx.mall.tiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tiny02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
