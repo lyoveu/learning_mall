2ba0d7a9939dbaef897002fee1ec5164eec43b2e
lyoveu <1228080240@qq.com>
2020-03-04 16:08:12 +0800

    tiny03 jwt，security

---
+++ tiny03/src/main/java/com/wx/mall/tiny/Tiny03Application.java
@@ -0,0 +1,13 @@
+package com.wx.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tiny03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tiny03Application.class, args);
+    }
+
+}
